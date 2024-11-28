89eab9d3059ae48f275465da98130ebefb612ff8
adk-saugat <adhikarisaugat34@gmail.com>
2024-11-27 19:21:51 -0600

    adding delete and check feature to tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,59 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
-  const [inputValue, setInputValue] = useState("");
+  const [inputVal, setInputval] = useState("");
   const [tasks, setTasks] = useState([]);
 
-  function handleClick() {
-    setTasks([...tasks, inputValue]);
-  }
+  const handleInputChange = (e) => {
+    setInputval(e.target.value);
+  };
+
+  const handleAddTask = (event) => {
+    setTasks([...tasks, { title: inputVal, id: uuidv4(), status: false }]);
+  };
+
+  const handleDelete = (taskId) => {
+    console.log(taskId);
+    const newTask = tasks.filter(({ id }) => {
+      return taskId !== id;
+    });
+    setTasks(newTask);
+  };
+
+  const handleChecked = (taskId) => {
+    setTasks(() => {
+      return tasks.map((task) => {
+        if (task.id === taskId) {
+          return { ...task, status: !task.status };
+        }
+        return { ...task };
+      });
+    });
+  };
+
   return (
     <>
-      <h2>Todo List</h2>
       <div>
-        <input
-          type="text"
-          placeholder="Add Task"
-          value={inputValue}
-          onChange={(event) => setInputValue(event.target.value)}
-        />
-        <button onClick={handleClick}>Add</button>
+        <input type="text" onChange={handleInputChange} />
+        <button onClick={handleAddTask}>Add</button>
       </div>
       <div>
-        {tasks.map((task) => (
-          <li>{task}</li>
-        ))}
+        {tasks.map(({ title, id, status }) => {
+          return (
+            <div key={id}>
+              <input
+                type="checkbox"
+                checked={status}
+                onChange={() => handleChecked(id)}
+              />
+              <span>{title}</span>
+              <button onClick={() => handleDelete(id)}>X</button>
+            </div>
+          );
+        })}
       </div>
     </>
   );
 }
-
 export default App;
